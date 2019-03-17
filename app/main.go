261e34accb3bfafdf8cded3e3c13d23333e803af
gravetii <dasika.sandy@gmail.com>
2019-03-17 12:03:27 +0530

    bufio read/write

--- app/main.go
+++ app/main.go
@@ -1,17 +1,47 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
-
-	"github.com/gravetii/gotest/http"
+	"log"
+	"os"
 )
 
 func main() {
-	resp := http.Response{Message: "default message", Code: 200}
-	fmt.Println(resp.Message)
-	fmt.Println(resp.Code)
+	file, err := os.Create("NewFile")
+	if err != nil {
+		panic(err)
+	}
 
-	req := http.Request{Header: "This is the header!", Body: "body"}
-	fmt.Println(req.Header)
-	fmt.Println(req.Body)
+	b := write(file)
+	log.Printf("Finished writing to file: %d", b)
+
+	rf, _ := os.Open("NewFile")
+	read(rf)
+
+	af, _ := os.OpenFile("NewFile", os.O_APPEND|os.O_WRONLY, 0600)
+	append(af)
+}
+
+func write(file *os.File) int {
+	buf := bufio.NewWriter(file)
+	b, _ := buf.WriteString("Writing data to buffer!\n")
+	buf.Flush()
+	file.Close()
+	return b
+}
+
+func read(file *os.File) {
+	reader := bufio.NewReader(file)
+	data, err := reader.Peek(50)
+	if err != nil {
+		fmt.Printf("Error: %v\n", err)
+	}
+
+	fmt.Println(string(data))
+}
+
+func append(file *os.File) {
+	file.WriteString("Trying to append!\n")
+	file.Close()
 }
